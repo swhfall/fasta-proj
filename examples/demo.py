b90203e9fb88cff0d3998236a00ea20b2aa7a325
swhfall <alenkamini5@gmail.com>
2025-10-07 20:54:00 +0300

    demo.py

--- examples/demo.py
+++ examples/demo.py
@@ -2,13 +2,27 @@ from fasta import Seq, FastaReader
 
 seq = Seq("test", "ATCGTUAACG")
 print(f"Длина: {len(seq)}")
-print(f"Тип: {seq.alphabet()}")
+print(f"Тип: {seq.get_alphabet()}")  
 
-reader_one = FastaReader("fasta-project/examples/small_example.fasta")
+print("\n" + "="*50)
+
+reader_one = FastaReader("examples/protein.faa")  
+print("Чтение protein.faa:")
+count = 0
 for sequence in reader_one:
     print(sequence)
+    count += 1
+    if count >= 2:  
+        break
 
-reader_two = FastaReader("fasta-project/examples/big_example.fasta")
+print("\n" + "="*50)
+
+reader_two = FastaReader("examples/GCA_000006945.2_ASM694v2_genomic.fna")  # Исправлен путь
+print("Чтение GCA_000006945.2_ASM694v2_genomic.fna:")
+count = 0
 for sequence in reader_two:
     print(sequence)
+    count += 1
+    if count >= 2:  
+        break
     
